3931822d7a381749a07303c07a0b57016b784088
FinNolofinwe <nolofinwe1881@gmail.com>
2020-11-03 22:25:43 +0300

    Second state with conditions and cycles

--- movie.js
+++ movie.js
@@ -33,13 +33,34 @@ const personalMovieDB = {
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
+/* const a = prompt('Один из последних просмотренных фильмов?', ''),
       b = prompt('На сколько оцените его?', ''),
       c = prompt('Один из последних просмотренных фильмов?', ''),
       d = prompt('На сколько оцените его?', '');
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+*/
 
-console.log(personalMovieDB);
+/*for ( let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+        if ( a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50 ) {
+          personalMovieDB.movies[a] = b;
+          console.log('done');
+        } else { 
+          console.log('error');
+          i--;
+        }       
+}
+*/
+if (personalMovieDB.count < 10 ) {
+  alert('Просмотрено довольно мало фильмов');
+} else if ( personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30 ) {
+  alert('Вы киноман');
+} else { alert('Произошла ошибка')}
+
 
 'use strict';
